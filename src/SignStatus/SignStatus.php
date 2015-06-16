958a029c7853b888cf05cfd31e496a53d5d04e0c
xpyctum <basalygaoleg@yandex.ru>
2015-06-16 18:48:13 +0500

    RU: - Новая система исчисления времени - Исправлен баг, когда после перезапуска сервера табличка переставала работать - Новые возможности: теперь можно настроить время обновления в конфиге, а перевод строк в другом файле конфигурации ! - На табличках появились цвета! EN: - New system for counting - Fixed bug, sign didn't updates when server restarted - New features! You can configure time for update sign or translate some lines for sign! - Now, signs has colors!

--- src/SignStatus/SignStatus.php
+++ src/SignStatus/SignStatus.php
@@ -1,117 +1,164 @@
 <?php
 namespace SignStatus;
 
-use pocketmine\level\Level;
 use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\Listener;
 use pocketmine\item\Item;
-use pocketmine\math\Vector3;
-use pocketmine\Player;
 use pocketmine\utils\Config;
 use pocketmine\plugin\PluginBase;
 use pocketmine\tile\Sign;
 use pocketmine\event\block\SignChangeEvent;
-use pocketmine\level\Position;
-use pocketmine\block\Block;
-use pocketmine\Server;
+use pocketmine\utils\TextFormat as F;
 
 /*
-##########################################
-###############by xpyctum#################
-##########################################
+         -        ████──██─██      -
+          -       █──██──███      -
+            -     ████────█      -
+              -   █──██───█    -
+                - ████────█  -
+                  ------------------
+██─██─████─██─██─████─███─█─█─█───█
+─███──█──█──███──█──█──█──█─█─██─██
+──█───████───█───█─────█──█─█─█─█─█
+─███──█──────█───█──█──█──█─█─█───█
+██─██─█──────█───████──█──███─█───█
 */
+//TODO: Make multisigns and configurable format of sign
 class SignStatus extends PluginBase implements Listener{
 
-	public $sign;
+    /** @var Config sign */
+    public $sign;
 
-	public function onEnable(){
-		if(!is_dir($this->getDataFolder())){
-			mkdir($this->getDataFolder());
-		}
-		$this->saveResource("sign.yml");
-		$this->sign = new Config($this->getDataFolder()."sign.yml", Config::YAML); //FIXED !
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$time = 100;
-		$this->getServer()->getScheduler()->scheduleRepeatingTask(new Task($this), $time);
-	}
-	public function onDisable()
-	{
-		
-	}
-	public function enabled(){
-		return $this->sign->get("sign")['enabled'];
-	}
-	public function level(){
-		return $this->sign->get("sign")['level'];
-	}
-	public function getThisSignX(){
-		return $this->sign->get("sign")['x'];
-	}
-	public function getThisSignY(){
-		return $this->sign->get("sign")['y'];
-	}
-	public function getThisSignZ(){
-		return $this->sign->get("sign")['z'];
-	}
-	public function onSignChange(SignChangeEvent $event){
-	$player = $event->getPlayer();
-		if(strtolower(trim($event->getLine(0))) == "status" || strtolower(trim($event->getLine(0))) == "[status]"){
-			if($player->hasPermission("signstatus")){
-				$tps = $this->getServer()->getTicksPerSecond();
-				$p = count($this->getServer()->getOnlinePlayers());
-				$level = $event->getBlock()->getLevel()->getName();
-				$full = $this->getServer()->getMaxPlayers();
-				$event->setLine(0,"[STATUS]");
-				$event->setLine(1,"TPS: [".$tps."]");
-				$event->setLine(2,"ONLINE: ".$p."/".$full."");
-				$event->setLine(3,"******");
-				$arr = array(
-				"sign" => array(
-					"x" => $event->getBlock()->getX(),
-					"y" => $event->getBlock()->getY(),
-					"z" => $event->getBlock()->getZ(),
-					"enabled" => true,
-					"level" => $level
-					)
-				);
-				$this->sign->setAll($arr);
-				$this->sign->save();
-				$this->sign->reload();
-				$event->getPlayer()->sendMessage("[SignStatus] You successfully created status sign!");
-			}else{
-				$player->sendMessage("[SignStatus] You don't have permissions!");
-				$event->setLine(0,"[SORRY]");
-				$event->setLine(1,"YOU");
-				$event->setLine(2,"DON'T HAVE");
-				$event->setLine(3,"PERMISSIONS");
-			}
-			
-		
-		}
-	}
-	public function onPlayerBreakBlock(BlockBreakEvent $event)
-    {
-        if ($event->getBlock()->getID() === Item::SIGN || $event->getBlock()->getID() === Item::WALL_SIGN || $event->getBlock()->getID() === Item::SIGN_POST) {
+    /** @var Config translation */
+    public $translation;
+
+    /** @var Config config */
+    public $config;
+
+    /** @var string  */
+    public $prefix = "§4[§2SignStatus§4]§6 ";
+
+    public function onEnable(){
+        if(!is_dir($this->getDataFolder())){
+            @mkdir($this->getDataFolder());
+            //Use default, not PM.
+        }
+
+        $this->saveResource("sign.yml");
+        $this->saveResource("translation.yml");
+        $this->saveResource("config.yml");
+
+        $this->sign = new Config($this->getDataFolder()."sign.yml", Config::YAML); //FIXED !
+        $this->translation = new Config($this->getDataFolder()."translation.yml",Config::YAML);
+        $this->config = new Config($this->getDataFolder()."config.yml",Config::YAML);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $time = $this->config->get("time");
+        if(!(is_numeric($time))){
+            $time = 20;
+            $this->getLogger()->alert("Can't read time for update sign! Please, check your config file! Default: ".F::AQUA." 1 ".F::WHITE." second");
+        }else{ $time = $time * 20; }
+        $this->getServer()->getScheduler()->scheduleRepeatingTask(new Task($this), $time);
+        $this->getLogger()->notice(F::GREEN."SignStatus loaded");
+
+    }
+
+    public function onDisable(){
+        $this->getLogger()->notice(F::RED."SignStatus disabled");
+    }
+
+    /**
+     * @deprecated
+     * @return mixed
+     */
+    public function enabled(){
+        return $this->sign->get("sign")['enabled'];
+    }
+    /**
+     * @deprecated
+     * @return mixed
+     */
+    public function level(){
+        return $this->sign->get("sign")['level'];
+    }
+    /**
+     * @deprecated
+     * @return mixed
+     */
+    public function getThisSignX(){
+        return $this->sign->get("sign")['x'];
+    }
+    /**
+     * @deprecated
+     * @return mixed
+     */
+    public function getThisSignY(){
+        return $this->sign->get("sign")['y'];
+    }
+    /**
+     * @deprecated
+     * @return mixed
+     */
+    public function getThisSignZ(){
+        return $this->sign->get("sign")['z'];
+    }
+
+
+    /**
+     * @param SignChangeEvent $event
+     */
+    public function onSignChange(SignChangeEvent $event){
+        $player = $event->getPlayer();
+        if(strtolower(trim($event->getLine(0))) == "status" || strtolower(trim($event->getLine(0))) == "[status]"){
+            if($player->hasPermission("signstatus")){
+                $tps = $this->getServer()->getTicksPerSecond();
+                $p = count($this->getServer()->getOnlinePlayers());
+                $level = $event->getBlock()->getLevel()->getName();
+                $full = $this->getServer()->getMaxPlayers();
+                $event->setLine(0,F::GREEN."[STATUS]");
+                $event->setLine(1,F::YELLOW."TPS: [".$tps."]");
+                $event->setLine(2,F::AQUA."ONLINE: ".F::GREEN.$p.F::WHITE."/".F::RED.$full."");
+                $event->setLine(3,F::GOLD."******");
+
+                $this->sign->setNested("sign.x", $event->getBlock()->getX());
+                $this->sign->setNested("sign.y", $event->getBlock()->getY());
+                $this->sign->setNested("sign.z", $event->getBlock()->getZ());
+                $this->sign->setNested("sign.enabled", true);
+                $this->sign->setNested("sign.level", $level);
+                $this->sign->save();
+                $this->sign->reload();
+                $event->getPlayer()->sendMessage($this->prefix.$this->translation->get("sign_created"));
+            }else{
+                $player->sendMessage($this->prefix.$this->translation->get("sign_no_perms"));
+                $event->setCancelled();
+            }
+        }
+    }
+
+    /**
+     * @param BlockBreakEvent $event
+     */
+    public function onPlayerBreakBlock(BlockBreakEvent $event){
+        if ($event->getBlock()->getID() == Item::SIGN || $event->getBlock()->getID() == Item::WALL_SIGN || $event->getBlock()->getID() == Item::SIGN_POST) {
             $signt = $event->getBlock();
             if (($tile = $signt->getLevel()->getTile($signt))){
-				if($tile instanceof Sign){
-				 if($event->getBlock()->getX() == $this->getThisSignX() || $event->getBlock()->getY() == $this->getThisSignY() || $event->getBlock()->getZ() == $this->getThisSignZ()){
-					$arr = array(
-					"sign" => array(
-						"x" => $event->getBlock()->getX(),
-						"y" => $event->getBlock()->getY(),
-						"z" => $event->getBlock()->getZ(),
-						"enabled" => false,
-						"level" => "world"
-					)
-				);
-				$this->sign->setAll($arr);
-				$this->sign->save();
-				$this->sign->reload();
-				 }
-				}
-			}
-		}
-	}
+                if($tile instanceof Sign) {
+                    if ($event->getBlock()->getX() == $this->sign->getNested("sign.x") || $event->getBlock()->getY() == $this->sign->getNested("sign.y") || $event->getBlock()->getZ() == $this->sign->getNested("sign.z")) {
+                        if($event->getPlayer()->hasPermission("signstatus.break")) {
+                            $this->sign->setNested("sign.x", $event->getBlock()->getX());
+                            $this->sign->setNested("sign.y", $event->getBlock()->getY());
+                            $this->sign->setNested("sign.z", $event->getBlock()->getZ());
+                            $this->sign->setNested("sign.enabled", false);
+                            $this->sign->setNested("sign.level", "world");
+                            $this->sign->save();
+                            $this->sign->reload();
+                            $event->getPlayer()->sendMessage($this->prefix.$this->translation->get("sign_destroyed"));
+                        }else{
+                            $event->getPlayer()->sendMessage($this->prefix.$this->translation->get("sign_no_perms"));
+                            $event->setCancelled();
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- src/SignStatus/Task.php
+++ src/SignStatus/Task.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace SignStatus;
+
 use pocketmine\scheduler\PluginTask;
-use pocketmine\Player; 
 use pocketmine\Server; 
-use pocketmine\level\Level; 
-use pocketmine\event\Listener;
+use pocketmine\level\Level;
 use pocketmine\math\Vector3;
 use pocketmine\tile\Sign;
+use pocketmine\utils\TextFormat as F;
 
 class Task extends PluginTask{
 	private $plugin;
@@ -19,21 +19,23 @@ class Task extends PluginTask{
 	}
 
     public function onRun($currentTick){
-    	$val = $this->plugin->enabled();;
-		if($val === "true" || $val === true){
-			$x = $this->plugin->getThisSignX();
-			$y = $this->plugin->getThisSignY();
-			$z = $this->plugin->getThisSignZ();
-			$lvz = $this->plugin->level();
+    	$val = $this->plugin->sign->get("sign")["enabled"];
+		if($val == "true" || $val == true){
+			$x = $this->plugin->sign->get("sign")["x"];
+			$y = $this->plugin->sign->get("sign")["y"];
+			$z = $this->plugin->sign->get("sign")["z"];
+			$lvz = $this->plugin->sign->get("sign")["level"];
 			$tps = Server::getInstance()->getTicksPerSecond();
 			$p = count(Server::getInstance()->getOnlinePlayers());
 			$full = Server::getInstance()->getMaxPlayers();
 			$level = Server::getInstance()->getLevelByName($lvz);
-			$sign = $level->getTile(new Vector3($x,$y,$z));
-			$count = $this->countable++; //For debug
-			if($sign instanceof Sign){
-				$sign->setText("[STATUS]", "TPS: [$tps]", "ONLINE: $p/$full", "$count");
-			}
+            if($level instanceof Level) {
+                $sign = $level->getTile(new Vector3($x, $y, $z));
+                $count = $this->countable++; //For debug
+                if ($sign instanceof Sign) {
+                    $sign->setText(F::GREEN."[STATUS]", F::YELLOW."TPS: [".$tps."]", F::AQUA."ONLINE: ".F::GREEN.$p.F::WHITE."/".F::RED.$full."", F::GOLD.$count);
+                }
+            }
 		}
     }
 }
